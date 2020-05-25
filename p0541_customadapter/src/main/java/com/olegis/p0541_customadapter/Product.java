3e7b7eb954d3b5071416bbb35671e53edfd53fb2
olegis85 <olegis85@mail.ru>
2020-05-25 12:07:16 +0300

    Кастомизация списка. Создаем свой адаптер на основе BaseAdapter

---
+++ p0541_customadapter/src/main/java/com/olegis/p0541_customadapter/Product.java
@@ -0,0 +1,15 @@
+package com.olegis.p0541_customadapter;
+
+public class Product {
+    String name;
+    int price;
+    int image;
+    boolean box;
+
+    public Product(String _describe, int _price, int _image, boolean _box) {
+        this.name = _describe;
+        this.price = _price;
+        this.image = _image;
+        this.box = _box;
+    }
+}
