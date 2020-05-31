91786fb57eecf737ea685e59710f1d7accf84215
olegis85 <olegis85@mail.ru>
2020-05-31 20:31:51 +0300

    Диалоги. ProgressDialog

---
+++ p0671_progressdialog/src/main/java/com/olegis/p0671_progressdialog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.olegis.p0671_progressdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
