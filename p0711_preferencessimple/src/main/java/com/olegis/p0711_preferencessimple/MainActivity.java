fc08694b1a59737ed46c8f2a6036640709ade4b0
olegis85 <olegis85@mail.ru>
2020-06-01 22:45:05 +0300

    Preferences как настройки приложения. PreferenceActivity

--- p0711_preferencessimple/src/main/java/com/olegis/p0711_preferencessimple/MainActivity.java
+++ p0711_preferencessimple/src/main/java/com/olegis/p0711_preferencessimple/MainActivity.java
@@ -2,13 +2,45 @@ package com.olegis.p0711_preferencessimple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvInfo = findViewById(R.id.tvInfo);
+
+        // получаем SharedPreferences, которое работает с файлом настроек
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        // полная очистка настроек
+        // sp.edit().clear().commit();
+    }
+
+    @Override
+    protected void onResume() {
+        Boolean notif = sp.getBoolean("notif", false);
+        String address = sp.getString("address", "");
+        String text = "Notifications are " + ((notif) ? "enadled, address = " + address : "disabled");
+        tvInfo.setText(text);
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
     }
 }
